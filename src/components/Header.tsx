afa8f6116da9fa19de7ebabbd4181e0c6ecde078
Qinisfighting <yq.qualmann@gmail.com>
2023-08-04 21:01:40 +0200

    drop down menu for small screen finish

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+import { useState, useEffect, useRef  } from "react"
 import { Link, NavLink } from "react-router-dom"
 import menu from "../../src/assets/menu.png"
+import home from "../../src/assets/home.png"
+import smile from "../../src/assets/smile.png"
+import project from "../../src/assets/project.png"
+import mail from "../../src/assets/mail.png"
 
 interface ActiveStyles {
     border: string;
@@ -7,35 +12,88 @@ interface ActiveStyles {
     boxShadow:string;
 }
 
+interface ActiveStylesMenu {
+    borderTop: string;
+    borderBottom: string;
+    width:string;
+}
+
+
 export default function Header() {
+    const [isMenu, setIsMenu] = useState<boolean>(false);
+    const menuRef = useRef<HTMLDivElement>(null);
+    
 
     const activeStyles: ActiveStyles = {
         border: "1px dotted #A49E9E",
         borderRadius: 50,
-        boxShadow: "1px 1px 2px gray"
+        boxShadow: "1px 1px 2px gray",
     }
+
+    const activeStylesMenu: ActiveStylesMenu = {
+        borderTop: "2px dotted white",
+        borderBottom: "2px dotted white",
+        width:"100%",
+        
+    }
+
+    useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            if(menuRef.current != null && !menuRef.current.contains(e.target as Node)){
+            setIsMenu(false)
+            console.log(menuRef.current);
+         }  
+        }
+
+        document.addEventListener("mousedown", handler)
+
+        return
+        () => {
+            document.removeEventListener("mousedown", handler)
+        }
+
+    })
+
+
+
 
     return (
         <header>
             <Link to='/' className="site-logo">Moin! Ich bin Qin <span className="qin">/'tʃɪn/. </span>
   </Link>
             <button className="lang">EN</button> 
-            <img src={menu} alt="menu" className="menu"/>
+            <img src={menu} alt="menu" className="menu" onClick={()=>setIsMenu(prev=>!prev)}/>
+           {isMenu ?
+            <div className="menuNav" ref={menuRef}>
+                <NavLink to='/' style={({isActive}: {isActive: boolean}) => isActive ? activeStylesMenu : null}>
+                   HOME <img src={home} alt="home" width="18px" />
+                </NavLink> 
+                <NavLink to='about' style={({isActive}: {isActive: boolean}) => isActive ? activeStylesMenu : null}>
+                  ICH <img src={smile} alt="smile" width="18px" />
+                </NavLink>
+                <NavLink to='works' style={({isActive}: {isActive: boolean}) => isActive ? activeStylesMenu : null}>
+                   PROJEKT <img src={project} alt="project" width="18px" />
+                </NavLink>   
+                <NavLink to='contact' className="login-link" style={({isActive}: {isActive: boolean}) => isActive ? activeStylesMenu : null}>
+                   KONTAKT <img src={mail} alt="mail" width="18px" />
+                </NavLink>    
+            </div>
+           :
             <nav>
                 <NavLink to='/' style={({isActive}: {isActive: boolean}) => isActive ? activeStyles : null}>
                    HOME
                 </NavLink>
                 <NavLink to='about' style={({isActive}: {isActive: boolean}) => isActive ? activeStyles : null}>
-                  ÜBER MICH
+                    ICH
                 </NavLink>
                 <NavLink to='works' style={({isActive}: {isActive: boolean}) => isActive ? activeStyles : null}>
-                   PROJEKTE
+                   PROJEKT
                 </NavLink>   
                 <NavLink to='contact' className="login-link" style={({isActive}: {isActive: boolean}) => isActive ? activeStyles : null}>
                    KONTAKT
                 </NavLink>    
             </nav>
-           
+        } 
         </header>
     )
 
