7b8ba3b47ebae12837e5c2884b3e718da46d7f96
Qinisfighting <yq.qualmann@gmail.com>
2023-08-09 20:55:09 +0200

    done

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,6 +2,8 @@ import { useLangContext } from "../components/Layout";
 import { useState, useEffect } from 'react';
 import github from "../assets/github.png";
 import upArrow from "../assets/upArrow.png";
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 export default function Projects(): JSX.Element {
 
@@ -20,7 +22,7 @@ interface ProjectsData extends Array<ProjectData>{}
 
 const {isGerman} = useLangContext()
 const [projectsData, setProjectsData] = useState<ProjectsData>([])
-const [isDisplay, setIsDisplay] = useState<boolean>(true)
+const [isDisplay, setIsDisplay] = useState<boolean>(false)
 const projectsDataURL = "https://raw.githubusercontent.com/Qinisfighting/my-portfolio/main/src/projectsData.json" 
 
 useEffect(() => {
@@ -29,35 +31,34 @@ useEffect(() => {
      const data = await response.json();
      setProjectsData(data)     
     // console.log(data)  
-   }
-   
+   }   
    fetchData();
-
 }, [])
 
 const projectElements = projectsData.map(project => {
- const {id, name, description_en, description_de, imageUrl,gitURL, appURL} = project
- return <div key={id} className="project-tile">
-          <div className="top">
-          <a target="_blank" href={appURL}><img className="project--img" src={imageUrl} alt="project-snapshot"/></a>
-          <div className="project-tile-top"> 
-            <div className="img--hover-text">APP ONLINE</div>   
-          </div>  
-          </div>   
-          <div className="project-tile-content">
-            <div className="project-tile-content-text">
-              <h3>{name}</h3>
-              <h4>{isGerman? description_de: description_en}</h4> 
-          </div>
-          <div className="bottom">
-          <a target="_blank" href={gitURL}><img src={github} className="git--icon"/></a>
-          <div className="project-tile-bottom">
-              <div className="icon--hover-text">SOURCE CODE</div>
-          </div>
-          </div> 
-          </div>
-        </div>
-}
+    const {id, name, description_en, description_de, imageUrl,gitURL, appURL} = project
+    return <div key={id} className={isDisplay? "each-slide-effect": "project-tile"}>
+             <div className="top">
+             <a target="_blank" href={appURL}><img className="project--img" src={imageUrl} alt="project-snapshot"/></a>
+             <div className="project-tile-top"> 
+               <div className="img--hover-text">APP ONLINE</div>   
+             </div>  
+             </div>   
+             <div className="project-tile-content">
+               <div className="project-tile-content-text">
+                 <h3>{name}</h3>
+                 <h4>{isGerman? description_de: description_en}</h4> 
+             </div>
+             <div className="bottom">
+             <a target="_blank" href={gitURL}><img src={github} className="git--icon"/></a>
+             <div className="project-tile-bottom">
+                 <div className="icon--hover-text">SOURCE CODE</div>
+             </div>
+             </div> 
+             </div>
+           </div>
+   }
+   
 
 )
 
@@ -66,27 +67,29 @@ function goTop(){
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
 }
 
-
-
 return (
     <>
-     { isDisplay?
-     <div className="projectsDisplay-container">
-         {projectElements}
-         <button onClick={()=>setIsDisplay(prev=>!prev)}>{isGerman?"ALLE":"All"}</button>
-    </div>
-    :
-    <div className="projectsAll-container">
-         <button className="go-back" onClick={()=>setIsDisplay(prev=>!prev)}>{isGerman?"ZURÜCK":"BACK"}</button>
-         {projectElements }
+      {isDisplay ? (
+        <div className="projectsDisplay-container">
+          <Slide indicators={true}>{projectElements}</Slide>
+  
+          <button className="all" onClick={() => setIsDisplay((prev) => !prev)}>
+            {isGerman ? "ALLE PROJEKTE" : "All PROJECTS"}
+          </button>
+        </div>
+      ) : (
+        <div className="projectsAll-container">
+          <button
+            className="go-back"
+            onClick={() => setIsDisplay((prev) => !prev)}
+          >
+            SLIDE SHOW
+          </button>
+          {projectElements}
           <img src={upArrow} className="go-top" onClick={goTop} />
-      
-         
-    </div>
-    } 
+        </div>
+      )}
     </>
-
-)
-
+  );
 }
 
