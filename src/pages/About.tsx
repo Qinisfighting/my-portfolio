a015f67b288f7cf2d1978e3f725b8478f690fc77
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-03-30 07:37:14 +0100

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -108,15 +108,17 @@ export default function About() {
             <p className="tags">
                 <span className="tag-span">JavaScript</span>
                 <span className="tag-span">TypeScript</span>
-                <span className="tag-span">React</span> 
+                <span className="tag-span">SQL</span>
+                <span className="tag-span">React</span>
+                <span className="tag-span">Next</span>
                 <span className="tag-span">Node</span>
                 <span className="tag-span">Express</span>  
                 <span className="tag-span">API/REST</span> 
+                <span className="tag-span">HTML6</span> 
                 <span className="tag-span">CSS(SCSS)</span>
                 <span className="tag-span">Tailwind</span>
                 <span className="tag-span">Figma</span>
                 <span className="tag-span">Firebase</span>
-                <span className="tag-span">MongoDB</span>
                 <span className="tag-span">Git</span>
                 <span className="tag-span">Agile</span>
                 . . .
