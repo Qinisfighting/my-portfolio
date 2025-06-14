73fd2bd544e5be6e9c3a5611d6fe5c1e23c6db32
Yanqin Qualmann <yq.qualmann@gmail.com>
2025-06-14 20:04:50 +0200

    add auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route
+  Route,
 } from "react-router-dom";
 import Layout from "./components/Layout";
 import Error from "./components/Error";
@@ -12,20 +12,18 @@ import Projects from "./pages/Projects";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Datenschutz from "./pages/Datenschutz";
+import BlogEntry from "./pages/BlogEntry";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route
-        path="projects"
-        element={<Projects />}
-        errorElement={<Error />}
-      />
+      <Route path="projects" element={<Projects />} errorElement={<Error />} />
       <Route path="services" element={<Services />} />
       <Route path="contact" element={<Contact />} />
       <Route path="datenschutz" element={<Datenschutz />} />
+      <Route path="blogEntry" element={<BlogEntry />} />
     </Route>
   )
 );
---
+++ src/pages/BlogEntry.tsx
@@ -0,0 +1,9 @@
+export default function BlogEntry() {
+  return (
+    <div className="blog-entry-container">
+      <h1>Blog Entry</h1>
+      <p>This is a placeholder for a blog entry.</p>
+      <p>More content will be added soon!</p>
+    </div>
+  );
+}
